bf8346130134c0f9775f74aebcfcf14b66b0aacd
khushal <khushalsingh12@gmail.com>
2022-11-28 20:10:58 +0530

    added middleware

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,11 +1,13 @@
 interface ConfigKeys {
     database: string,
     port: number,
-    logDir: string
+    logDir: string,
+    jwtKey: string
 }
 
 export const config: ConfigKeys = {
     database: process.env.DATABASE,
     port: Number(process.env.PORT) || 4001,
-    logDir: process.env.APP_DIR
+    logDir: process.env.APP_DIR,
+    jwtKey: process.env.JWT_KEY
 }
