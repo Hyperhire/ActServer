834fb7a7f2ea2d2e81a8489c3153017779460d50
khushal <khushalsingh12@gmail.com>
2022-11-29 11:55:30 +0530

    merged with remote

--- src/middleware/validate.middleware.ts
+++ src/middleware/validate.middleware.ts
@@ -3,16 +3,13 @@ import { Request, Response } from "express";
 import { validationError } from '../common/dto/response.dto'
 import { logger } from '../logger/winston.logger'
 
-const validateBody = <T>() => {
-    return (req: Request, res: Response, next: Function)=> {
-        try {
-            const validatedBody = validateOrReject(req.body)
-            next()
-        } catch (error) {
-            logger.error(error)
-            res.status(401).json(validationError)
-        }
+const validateBody = async (req: Request, res: Response, next: Function) => {
+    try {
+        const validatedBody = await validateOrReject(req.body)
+        next()
+    } catch (error) {
+        logger.error(error)
+        res.status(401).json(validationError)
     }
 }
-
 export { validateBody }
