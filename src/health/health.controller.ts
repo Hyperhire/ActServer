eb2a46087941e57a6eb0783719dbc8ae6faf6ff9
conan-kim <conan.kim@hyperhire.in>
2023-01-31 13:02:12 +0900

    modified: health test

---
+++ src/health/health.controller.ts
@@ -0,0 +1,30 @@
+import { Request, Router, Response } from "express";
+import multer from "multer";
+import KasWallet from "../utils/kasWallet";
+import { getRedisValueByKey } from "../utils/redis";
+import { uploadFile } from "../utils/upload";
+
+const router = Router();
+const upload = multer();
+
+interface MulterRequest extends Request {
+    file: any;
+    files: any;
+}
+
+router.get("/", (request: Request, response: Response) => {
+    try {
+        return response.json({
+            status: 200,
+            data: {
+                text: "hello, this is conan from hyperhire",
+                timestamp: new Date(),
+            },
+        });
+    } catch (error) {
+        return response.json({ status: 400, error });
+    }
+});
+
+
+export default router;
